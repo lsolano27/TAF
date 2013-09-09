f09477dec1f2dc2a63ac787d0ad14b411191c731
cguillen <cguilen@testingsoft.com>
2013-09-09 09:44:20 -0500

    Ejemplos

--- src/test/java/com/ts/Shell/MiPc.java
+++ src/test/java/com/ts/Shell/MiPc.java
@@ -1,11 +1,38 @@
 package com.ts.Shell;
 
+import org.testng.Assert;
+
 import com.ts.commons.Component;
 import com.ts.commons.Shell;
 
 public class MiPc extends Shell{
-
 	
+	private boolean pingIsOk = false;
+	
+	public MiPc()
+	{
+		
+	}
+	
+	public MiPc validateExpectedOutPut() {
+		Assert.assertEquals(pingIsOk, true, "ping comand was failed");
+		return this;
+	}
+
+	public MiPc connect(String ip, String user, String password)
+	{
+		super.connect(ip, user, password, user);
+		setSkipVT100Filter(true);
+		open();
+		return this;
+	}
+	
+	public MiPc sendPingCommandToGateWay()
+	{
+		execute("ping 10.0.0.1", "Approximate");
+		pingIsOk = true;
+		return this;
+	}
 	
 	public MiPc and() {
 		return this;
@@ -15,9 +42,5 @@ public class MiPc extends Shell{
 		return this;
 	}
 
-	public static Object crearSession() {
-		
-		return null;
-	}
 
 }
--- src/test/java/com/ts/Shell/TestCase.java
+++ src/test/java/com/ts/Shell/TestCase.java
@@ -1,6 +1,7 @@
 package com.ts.Shell;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 
 import com.ts.commons.TestCaseUtil;
 import com.ts.commons.Validator;
@@ -10,23 +11,17 @@ public class TestCase extends TestCaseUtil{
 	@Test
 	public void test()
 	{
+		String user = "root"; /*Valid user*/
+		String linux ="127.0.0.1";/*valid ip*/
+		String password ="";/*valid password*/
+		
 		MiPc miPc = new MiPc();
 		using(
 				miPc.
-				connect("10.0.0.35","root", "TesterUser", "root").
-				setSkipVT100Filter(true).
-				open().
-				execute("ping 10.0.0.1", "Approximate")
+				connect(linux,user, password, user)
 		).
 		check(
-				new Validator() {
-					
-					@Override
-					public void Validate() {
-						// TODO Auto-generated method stub
-						
-					}
-				}
+				miPc.validateExpectedOutPut()
 		);
 		
 		
