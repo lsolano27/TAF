33ca6ad0172b376ee16cecf5af87b1d6b815b582
cguillen <cguilen@testingsoft.com>
2013-10-03 12:17:28 -0500

    Se suben fixes el chrome driver

--- src/test/java/com/ts/example/email/TestCase.java
+++ src/test/java/com/ts/example/email/TestCase.java
@@ -14,8 +14,8 @@ public class TestCase extends TestCaseUtil{
 	@Test
 	public void test() throws MessagingException, IOException, ParseException
 	{
-		String account ="cguillen@testingsoft.com";/*valid accouunt@gmail.com.*/
-		String password = "cguillen123";/*proper password.*/
+		String account ="";/*valid accouunt@gmail.com.*/
+		String password = "";/*proper password.*/
 		
 		MyGmail gmail = new MyGmail(account, password);
 		
