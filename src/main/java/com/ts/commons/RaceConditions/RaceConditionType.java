a61efac8a7f184cb4dd38577aade47180e4cc266
ovaleriotestingsoft <ovalerio@testingsoft.com>
2014-04-24 14:08:18 -0600

    Race Condition Type enum added

---
+++ src/main/java/com/ts/commons/RaceConditions/RaceConditionType.java
@@ -0,0 +1,8 @@
+package com.ts.commons.RaceConditions;
+
+public interface RaceConditionType {
+	String VAADIN = "Vaadin";
+	String JQUERY = "Jquery";
+	String JAVASCRIPT = "JavaScript";
+	String AJAX = "Ajax";
+}
